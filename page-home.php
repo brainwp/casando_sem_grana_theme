acef717f4ca9aa1ddd185441408a8db58067a45b
everaldomatias <matiaseveraldo@gmail.com>
2014-04-11 15:11:42 -0300

    Exibindo as opcoes do Theme Options na Page Home
    Aqui pegamos todos os valores em variáveis, aplicamos em suas div's e aplicamos o devido CSS

--- page-home.php
+++ page-home.php
@@ -6,30 +6,60 @@
 
 <?php get_header(); ?>
 
-<section id="body_slides"><h1>Slides - Plugin</h1>
-	<div id="boxes">
-		<article id="box-1" class="box">
-			<span class="img-bullet"></span>
-			<h1 class="title"></h1>
-			<div class="excerpt"></div>
-		</article>
-		<article id="box-2" class="box">
-			<span class="img-bullet"></span>
-			<h1 class="title"></h1>
-			<div class="excerpt"></div>
-		</article>
-		<article id="box-3" class="box">
-			<span class="img-bullet"></span>
-			<h1 class="title"></h1>
-			<div class="excerpt"></div>
-		</article>
-		<article id="box-4" class="box">
-			<span class="img-bullet"></span>
-			<h1 class="title"></h1>
-			<div class="excerpt"></div>
-		</article>
-	</div><!-- #boxes -->
+<section id="body_slides">
+	<h1>Slides - Plugin</h1>
 </section><!-- #body_slides -->
+
+<?php
+	//Box 1
+	$icone_box_1 = of_get_option('icon_feature_upload_um');
+	$title_box_1 = of_get_option('title_feature_um');
+	$excerpt_box_1 = of_get_option('resumo_feature_um');
+
+	//Box 2
+	$icone_box_2 = of_get_option('icon_feature_upload_dois');
+	$title_box_2 = of_get_option('title_feature_dois');
+	$excerpt_box_2 = of_get_option('resumo_feature_dois');
+
+	//Box 3
+	$icone_box_3 = of_get_option('icon_feature_upload_tres');
+	$title_box_3 = of_get_option('title_feature_tres');
+	$excerpt_box_3 = of_get_option('resumo_feature_tres');
+
+	//Box 4
+	$icone_box_4 = of_get_option('icon_feature_upload_quatro');
+	$title_box_4 = of_get_option('title_feature_quatro');
+	$excerpt_box_4 = of_get_option('resumo_feature_quatro');
+?>
+
+<div id="boxes" class="boxes-home">
+	<article id="box-1" class="box">
+		<img src="<?php echo $icone_box_1; ?>" alt="<?php echo $title_box_1; ?>" />
+		<h2 class="title"><?php echo $title_box_1; ?></h2>
+		<div class="excerpt"><?php echo $excerpt_box_1; ?></div>
+	</article>
+	<article id="box-2" class="box">
+		<span class="img-bullet">
+			<img src="<?php echo $icone_box_2; ?>" alt="<?php echo $title_box_2; ?>" />
+		</span>
+		<h1 class="title"><?php echo $title_box_2; ?></h1>
+		<div class="excerpt"><?php echo $excerpt_box_2; ?></div>
+	</article>
+	<article id="box-3" class="box">
+		<span class="img-bullet">
+			<img src="<?php echo $icone_box_3; ?>" alt="<?php echo $title_box_3; ?>" />
+		</span>
+		<h1 class="title"><?php echo $title_box_3; ?></h1>
+		<div class="excerpt"><?php echo $excerpt_box_3; ?></div>
+	</article>
+	<article id="box-4" class="box">
+		<span class="img-bullet">
+			<img src="<?php echo $icone_box_4; ?>" alt="<?php echo $title_box_4; ?>" />
+		</span>
+		<h1 class="title"><?php echo $title_box_4; ?></h1>
+		<div class="excerpt"><?php echo $excerpt_box_4; ?></div>
+	</article>
+</div><!-- #boxes -->
 
 <section class="body_menu-home">
 	<nav class="nav-menu-toogle">
