fc23c4d94e5c176d5ce89a00ffe822ddf4178f2c
Diego Rojas <diego@brasa.art.br>
2014-04-17 17:41:08 -0300

    começando a resolver o header

--- header.php
+++ header.php
@@ -27,8 +27,18 @@
 <body <?php body_class(); ?>>
 <div id="page" class="hfeed site">
 
-	<header id="masthead" class="site-header" role="banner">
-		<section class="section-header">
+	<header id="masthead" class="site-header" role="banner">		<section class="section-header">
+
+			<div id="img-header-left">
+			</div><!-- #img-header-left -->
+			
+					<h1 class="site-title">
+						<a href="<?php echo esc_url( home_url( '/' ) ); ?>" title="<?php bloginfo( 'name' ); ?>" rel="home">
+							<img id="logo" src="<?php header_image(); ?>" height="<?php echo get_custom_header()->height; ?>" width="<?php echo get_custom_header()->width; ?>" alt="" />
+						</a>
+					</h1>
+
+			<div id="img-header-right"></div><!-- #img-right -->
 
 			<div class="box-rss">
 				<h2><a href="<?php echo home_url( '/feed' ); ?>" target="_blank"><?php bloginfo( 'description' ); ?></a></h2>
@@ -38,28 +48,6 @@
 			<a class="conh-morando" href="http://www.morandosemgrana.com.br" target="_blank">
 			</a><!-- .conh-morando -->
 
-			<div id="img-header-left">
-			</div><!-- #img-header-left -->
-			
-				<div class="site-branding">
-					<h1 class="site-title">
-						<a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home">
-							<?php //bloginfo( 'name' ); ?>
-							<!-- <img  src="<?php //bloginfo( "template_url" ); ?>/images/logo.png"> -->
-							<img id="logo" src="<?php header_image(); ?>" height="<?php echo get_custom_header()->height; ?>" width="<?php echo get_custom_header()->width; ?>" alt="" />
-						</a>
-					</h1>
-					
-				</div><!-- .site-branding -->
-
-			<div id="img-header-right"></div><!-- #img-right -->
-		</section><!-- .section-header -->
-
-		<nav id="site-navigation" class="main-navigation" role="navigation">
-			<h1 class="menu-toggle"><?php //_e( 'Menu', 'casando_sem_grana_theme' ); ?></h1>
-			<a class="skip-link screen-reader-text" href="#content"><?php //_e( 'Skip to content', 'casando_sem_grana_theme' ); ?></a>
-			<?php //wp_nav_menu( array( 'theme_location' => 'primary' ) ); ?>
-		</nav><!-- #site-navigation -->
 	</header><!-- #masthead -->
 
 	<div id="content" class="site-content">
