977948270694d004f2ccf644ee38e9a7223b7246
diegorojas <diego@brasa.art.br>
2014-04-17 11:36:07 -0300

    subindo functions alterada para implementar neslleter

--- functions.php
+++ functions.php
@@ -140,6 +140,21 @@ function casando_sem_grana_theme_widgets_init() {
 add_action( 'widgets_init', 'casando_sem_grana_theme_widgets_init' );
 
 /**
+ * Register widgetized area and update sidebar with default widgets.
+ */
+function casando_sem_grana_theme_widgets_newsletter() {
+	newsletter_footer( array(
+		'name'          => __( 'Newsletter', 'casando_sem_grana_theme' ),
+		'id'            => 'newsletter-footer',
+		'before_widget' => '<aside id="%1$s" class="widget %2$s">',
+		'after_widget'  => '</aside>',
+		'before_title'  => '<h1 class="widget-title">',
+		'after_title'   => '</h1>',
+	) );
+}
+add_action( 'widgets_init', 'casando_sem_grana_theme_widgets_newsletter' );
+
+/**
  * Enqueue scripts and styles.
  */
 function casando_sem_grana_theme_scripts() {
