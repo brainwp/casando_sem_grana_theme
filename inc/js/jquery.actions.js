471bd55fe78a319003f44f5ee5063e932418edc3
diegorojas <diego@brasa.art.br>
2014-04-23 15:30:02 -0300

    Css arrumado das subcategorias na home e manu das categorias pais e filhas também arrumado na category.php

--- inc/js/jquery.actions.js
+++ inc/js/jquery.actions.js
@@ -17,3 +17,17 @@ $(function() {
     });
 });
 /* END | Menu de Categorias */
+
+/* START | Menu de Categorias */
+$(function() {
+    //$('ul.sub-category-hide li').hide();
+    var $links = $('.nav-menu-toogle .active-toggle');
+    $links.click(function(){
+        $('.sub-category-hide').fadeOut(300);
+        //$links.removeClass('selected');
+        $('ul#child-' + $(this).data('category')).slideToggle('slow');
+        //$(this).addClass('selected');
+        //$('ul#child-' + $(this).data('category')).toggleClass();
+    });
+});
+/* END | Menu de Categorias */
