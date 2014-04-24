6af9b3f0defe6c4f151e53279df5bfe6944d5efd
jonyfernandoschulz <diego@brasa.art.br>
2014-04-24 15:06:39 -0300

    Campo busca da side bar page arrumado

---
+++ sidebar-page.php
@@ -0,0 +1,30 @@
+<?php
+/**
+ * The Sidebar containing the main widget areas.
+ *
+ * @package casando_sem_grana_theme
+ */
+?>
+<section id="side-single">	
+	<div id="secondary" class="widget-area" role="complementary">
+		<?php if ( ! dynamic_sidebar( 'sidebar-page' ) ) : ?>
+
+		page
+
+			<aside id="search" class="widget widget_search">
+				<?php get_search_form(); ?>
+			</aside><!-- #search -->
+
+			<aside id="history" class="widget">
+				<h2 class="widget-title">Conte-nos a sua História</h2>
+				<div id="th-history"></div>
+			</aside><!-- #history -->
+
+			<aside id="video" class="widget">
+				<h2 class="widget-title">Conheça nosso canal no Youtube</h2>
+				<div id="th-video"><a href=""></a></div>
+			</aside><!-- #video -->
+
+		<?php endif; // end sidebar widget area ?>
+	</div><!-- #secondary -->
+</section><!-- #side-single -->
