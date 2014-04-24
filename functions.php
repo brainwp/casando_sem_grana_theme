3998718ee4870e2937f9642216d2b60e8b9d6e76
everaldomatias <matiaseveraldo@gmail.com>
2014-04-24 12:49:01 -0300

    Arrumando div aberta nos comentarios

--- functions.php
+++ functions.php
@@ -337,7 +337,7 @@ function custom_comments( $comment, $args, $depth ) {
 		$add_below = 'div-comment';
 	}
 ?>
-	<?php echo $tag ?> <?php comment_class( empty( $args['has_children'] ) ? '' : 'parent' ) ?> id="comment-<?php comment_ID() ?>">
+	<<?php echo $tag; ?> <?php comment_class( empty( $args['has_children'] ) ? '' : 'parent' ); ?> id="comment-<?php comment_ID(); ?>">
 	<?php if ( 'div' != $args['style'] ) : ?>
 	<div id="div-comment-<?php comment_ID() ?>" class="comment-body">
 	<?php endif; ?>
