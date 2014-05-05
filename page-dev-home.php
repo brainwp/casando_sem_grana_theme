fc3602ce24b9d30f064abda4b87f6c58c12ee36a
everaldomatias <matiaseveraldo@gmail.com>
2014-05-05 16:21:37 -0300

    Finaliza categorias da Home dinamicamente e adiciona essa section em content-menu-category.php

--- page-dev-home.php
+++ page-dev-home.php
@@ -74,8 +74,11 @@
 </div><!-- #boxes -->
 
 <section class="body_menu-home">
-	<?php $to_loop = array();
-	$i = ''; ?>
+	<?php
+		$to_loop = array();
+		$i = '';
+		$c = '1';
+	?>
 	<nav class="nav-menu-toogle">
 		<ul>
 			<?php
@@ -93,48 +96,56 @@
 				'taxonomy'                 => 'category',
 				'pad_counts'               => false );
 
+				// Pega as categorias Pai
 				$categories = get_categories( $args );
-				foreach( $categories as $category ) :
-				$cat_ID = $category->term_id; // Get ID the category.
-			?>
-				<li class="active-toggle">
-						<div class="ring">
-							<span class="img-cat parent-item-<?php echo $cat_ID; ?>"></span>
-								<?php echo $category->name; ?>'
-							<div class="hover-cat"></div>
-						</div>
 
-						<?php	
-							$child_args = array( 'child_of' => $cat_ID, );
-							$child_categories = get_categories( $child_args );
-						?>
-					<ul class="sub-category-hide">
-						<li>
-							
-							<?php foreach( $child_categories as $child_category ) : ?>		
-								<?php $to_loop[$i] .= $child_category->parent; ?>
-								<?php $i++; ?>
-							<?php endforeach; ?>
-						</li>
-					</ul>
+				//Loop nas categorias Pai				
+				foreach( $categories as $category ) :
+				
+				// Guarda o ID das categorias Pai
+				$cat_ID = $category->term_id;
+			?>
+			
+				<!-- HTML impresso pelo loop acima -->	
+				<li data-category="<?php echo $cat_ID; ?>" class="active-toggle">
+					<div class="ring">
+						<span class="img-cat parent-item-<?php echo $cat_ID; ?> parent-image-<?php echo $c; ?>"></span>
+							<?php echo $category->name; ?>'
+						<div class="hover-cat"></div>
+					</div>
+					
+					<?php $c++; ?>
+					
+					<?php	
+						$child_args = array( 'child_of' => $cat_ID, );
+						
+						// Pega as categorias Filhas
+						$child_categories = get_categories( $child_args );
+					?>
+						<!-- Guarda no array $to_loop o ID das categorias Filhas-->
+						<?php foreach( $child_categories as $child_category ) : ?>		
+							<?php $to_loop[$i] .= $child_category->parent; ?>
+							<?php $i++; ?>
+						<?php endforeach; ?>
 				</li>
 					
 	<?php endforeach; ?>
 				
 		</ul>
+
+			<!-- HTML impresso para as categorias Filhas -->	
+			<?php foreach( $to_loop as $to ) : ?>		
+				<?php $categories = get_categories( "hide_empty=0&parent=$to" );
+			        echo '<ul id="child-'. $to .'" class="sub-category-hide">';
+			        foreach ( $categories as $cat ) {
+						echo '<li><a href="' . get_category_link( $cat->term_id ) . '" >' . $cat->name . '</a></li>';
+			        }
+			        echo '</ul>'; ?>
+			<?php endforeach; ?>
+
 	</nav><!-- .nav-menu-home -->
 
 </section><!-- .body_menu-home -->
-
-<?php foreach( $to_loop as $to ) : ?>		
-	<?php  $categories = get_categories( "hide_empty=0&parent=$to" );
-
-        echo '<ul class="hide">';
-        foreach ( $categories as $cat ) {
-                echo '<li><a href="' . get_category_link( $cat->term_id ) . '" >' . $cat->name . '</a></li>';
-        }
-        echo '</ul>'; ?>
-<?php endforeach; ?>
 
 <section id="box-content">
 	<div class="anuncio">
